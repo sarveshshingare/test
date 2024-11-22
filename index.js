265396aefd12060f50270ea15c06f80dbf4de167
root <root@ip-172-31-3-23.ap-south-1.compute.internal>
2024-11-22 14:04:24 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const http = require("http");
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello from Node.js app!");
+});
+server.listen(3000);
+
